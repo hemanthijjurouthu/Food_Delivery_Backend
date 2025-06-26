f75c46567e5395c575796d8611e5def77917b1c1
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-06-26 09:31:04 +0530

    added vercel.json

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
+dotenv.config();
+
 const app = express();
 
-//middlewares
 app.use(express.json());
 app.use(cors());
 
-//db connection
 connectDB();
 
-//api endpoint
-app.use("/api/food",foodRouter);
-app.use("/images",express.static('uploads'));
-app.use("/api/user",userRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/order",orderRouter);
+// API routes
+app.use("/api/food", foodRouter);
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get("/",(req,res) => {
-    res.send("API WORKING");
-})
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server started on http://localhost:${process.env.PORT}`);
-})
+app.use("/images", express.static("uploads"));
+
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
