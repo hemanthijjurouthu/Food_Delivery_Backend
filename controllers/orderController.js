39d1f48af0e0e71c423057989b0f575481856a6a
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-06-26 09:40:12 +0530

    added urls

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = process.env.FRONTEND_URL;
+  const frontend_url = "http://localhost:5174";
 
   try {
     // Save the order to the database
